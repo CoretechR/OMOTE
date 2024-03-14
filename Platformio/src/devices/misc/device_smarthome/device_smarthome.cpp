ac29d4936be250cb24d9ba3d2f718ad492751b97
KlausMu <kmusch@gmail.com>
2024-03-14 14:43:33 +0100

    enable smarthome when wifi is enabled, not when mqtt keyboard is enabled

--- Platformio/src/devices/misc/device_smarthome/device_smarthome.cpp
+++ Platformio/src/devices/misc/device_smarthome/device_smarthome.cpp
@@ -7,7 +7,7 @@ uint16_t SMARTHOME_MQTT_BULB1_BRIGHTNESS_SET ; //"Smarthome_mqtt_bulb1_brightnes
 uint16_t SMARTHOME_MQTT_BULB2_BRIGHTNESS_SET ; //"Smarthome_mqtt_bulb2_brightness_set";
 
 void register_device_smarthome() {
-  #if (ENABLE_KEYBOARD_MQTT == 1)
+  #if (ENABLE_WIFI_AND_MQTT == 1)
   register_command(&SMARTHOME_MQTT_BULB1_SET            , makeCommandData(MQTT, {"bulb1_set"             })); // payload must be set when calling commandHandler
   register_command(&SMARTHOME_MQTT_BULB2_SET            , makeCommandData(MQTT, {"bulb2_set"             })); // payload must be set when calling commandHandler
   register_command(&SMARTHOME_MQTT_BULB1_BRIGHTNESS_SET , makeCommandData(MQTT, {"bulb1_setbrightness"   })); // payload must be set when calling commandHandler
