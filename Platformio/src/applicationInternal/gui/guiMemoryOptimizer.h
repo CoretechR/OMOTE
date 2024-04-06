e7c81cd5f5b0fbced3c5a5e2253d61e12f801951
JustMe-NL <JustMe-NL@users.noreply.github.com>
2024-04-06 11:14:12 +0200

    Update guiMemoryOptimizer.h
    Added 2 functions to assist in updating widgets after a new MQTT message

--- Platformio/src/applicationInternal/gui/guiMemoryOptimizer.h
+++ Platformio/src/applicationInternal/gui/guiMemoryOptimizer.h
@@ -2,3 +2,5 @@
 
 void gui_memoryOptimizer_prepare_startup();
 void gui_memoryOptimizer_doAfterSliding_deletionAndCreation(lv_obj_t** tabview, int oldTabID, int newTabID, bool newGuiList, lv_obj_t** panel, lv_obj_t** img1, lv_obj_t** img2);
+bool checkTabActive(std::string tabName);
+bool checkTabinMemory(std::string tabName);
