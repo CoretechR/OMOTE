5e002dfe9936113a69953107bf4520bb8b88e684
MatthewColvin <mcolvin32.7@gmail.com>
2024-08-03 01:14:52 -0500

    add ability to set text on button

--- Platformio/OmoteUI/core/widget/Button.cpp
+++ Platformio/OmoteUI/core/widget/Button.cpp
@@ -4,7 +4,8 @@
 using namespace UI::Widget;
 
 Button::Button(std::function<void()> aOnPressHandler)
-    : Base(lv_btn_create(UI::Screen::BackgroundScreen::getLvInstance()),ID::Widgets::Button),
+    : Base(lv_btn_create(UI::Screen::BackgroundScreen::getLvInstance()),
+           ID::Widgets::Button),
       mOnPress(aOnPressHandler) {}
 
 void Button::OnLvglEvent(lv_event_t *anEvent) {
@@ -12,3 +13,11 @@ void Button::OnLvglEvent(lv_event_t *anEvent) {
     mOnPress();
   }
 };
+
+void Button::SetText(std::string aText) {
+  if (!mText) {
+    mText = AddNewElement<Label>(aText);
+    mText->AlignTo(this, LV_ALIGN_CENTER);
+  }
+  mText->SetText(aText);
+}
--- Platformio/OmoteUI/core/widget/Button.hpp
+++ Platformio/OmoteUI/core/widget/Button.hpp
@@ -1,14 +1,19 @@
 #pragma once
+#include "Label.hpp"
 #include "WidgetBase.hpp"
-
 namespace UI::Widget {
 class Button : public Base {
 public:
   Button(std::function<void()> aOnPressHandler);
+  virtual ~Button() = default;
 
+  void SetText(std::string aText);
+
+protected:
   void OnLvglEvent(lv_event_t *anEvent) override;
 
 private:
+  Label *mText = nullptr;
   std::function<void()> mOnPress;
 };
 
