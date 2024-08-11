218570170df6b815f240c89ee77e75cffeaff508
MatthewColvin <mcolvin32.7@gmail.com>
2024-08-10 20:59:33 -0500

    Update API to used defines as default params Add check to see if server is running.

--- Platformio/HomeAssistant/Api.cpp
+++ Platformio/HomeAssistant/Api.cpp
@@ -1,44 +1,67 @@
 #include "Api.hpp"
 
+#include <memory>
 using namespace HomeAssistant;
 
-Api::Api() { curl_global_init(CURL_GLOBAL_DEFAULT); }
+Api::Api(std::string aIp, std::string aPort) : mIp(aIp), mPort(aPort) {
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+}
 
 Api::~Api() { curl_global_cleanup(); }
 
-rapidjson::Document Api::doStuff() {
-  mCurlHandle = curl_easy_init();
-  struct curl_slist *headers = NULL; // List to hold headers
+bool Api::IsApiRunning() {
+  auto res = SendRequest();
 
-  headers = curl_slist_append(headers, jsonHeader);
-  headers = curl_slist_append(headers, getSecurityHeader().c_str());
+  if (res.HasMember("message") && res["message"].IsString()) {
+    return strcmp(res["message"].GetString(), "API running.") == 0;
+  }
+  return false;
+}
 
-  curl_easy_setopt(mCurlHandle, CURLOPT_URL, getURL().c_str());
-  curl_easy_setopt(mCurlHandle, CURLOPT_HTTPHEADER, headers);
-  curl_easy_setopt(mCurlHandle, CURLOPT_WRITEFUNCTION, Api::write_callback);
-  curl_easy_setopt(mCurlHandle, CURLOPT_WRITEDATA, this);
-  auto code = curl_easy_perform(mCurlHandle);
+rapidjson::Document Api::SendRequest(std::string anApiPath) {
+  // Curl Easy Init
+  auto curlEasyCleanup = [&](CURL *aFinishedCurl) {
+    curl_easy_cleanup(aFinishedCurl);
+  };
+  std::unique_ptr<CURL, decltype(curlEasyCleanup)> easyCurl(curl_easy_init(),
+                                                            curlEasyCleanup);
+
+  // Curl Create Headers
+  struct curl_slist *headersPtr = NULL;
+  headersPtr = curl_slist_append(headersPtr, jsonHeader);
+  headersPtr = curl_slist_append(headersPtr, getSecurityHeader().c_str());
+
+  auto listDeleter = [&](curl_slist *ptr) { curl_slist_free_all(ptr); };
+  std::unique_ptr<curl_slist, decltype(listDeleter)> headers(headersPtr,
+                                                             listDeleter);
+
+  // Put together Curl Request
+  curl_easy_setopt(easyCurl.get(), CURLOPT_URL, getURL(anApiPath).c_str());
+  curl_easy_setopt(easyCurl.get(), CURLOPT_HTTPHEADER, headers.get());
+  curl_easy_setopt(easyCurl.get(), CURLOPT_WRITEFUNCTION,
+                   Api::HandleResultCallback);
+  curl_easy_setopt(easyCurl.get(), CURLOPT_WRITEDATA, this);
+  auto code = curl_easy_perform(easyCurl.get());
 
   printf("Curl Code:%d", code);
 
   rapidjson::Document d;
   d.Parse(mResponse.c_str());
 
-  curl_slist_free_all(headers);
-  curl_easy_cleanup(mCurlHandle);
-
   return d;
 }
 
 std::string Api::getSecurityHeader() {
-  return "Authorization: Bearer " + std::string(token);
+  return "Authorization: Bearer " + std::string(HOMEASSISTANT_API_TOKEN);
 }
 
-std::string Api::getURL() { return "http://" + mIp + ":" + mPort + "/api/"; }
+std::string Api::getURL(std::string aPath) {
+  return "http://" + mIp + ":" + mPort + "/api/" + aPath;
+}
 
 // Static
-size_t Api::write_callback(void *contents, size_t size, size_t nmemb,
-                           void *userp) {
+size_t Api::HandleResultCallback(void *contents, size_t size, size_t nmemb,
+                                 void *userp) {
   size_t total_size = size * nmemb;
   printf("%s", (char *)contents); // Print the received data
 
--- Platformio/HomeAssistant/Api.hpp
+++ Platformio/HomeAssistant/Api.hpp
@@ -1,33 +1,39 @@
 #pragma once
 #include "curl/curl.h"
 #include "rapidjson/document.h"
+#include <memory>
 #include <string>
+
 namespace HomeAssistant {
 
 class Api {
 public:
-  Api();
+  Api(std::string aIp = HOMEASSISTANT_IP_ADDRESS,
+      std::string aPort = HOMEASSISTANT_PORT);
   virtual ~Api();
 
-  rapidjson::Document doStuff();
+  bool IsApiRunning();
+  rapidjson::Document SendRequest(std::string anApiPath = "");
 
 private:
   static constexpr auto jsonHeader = "Content-Type: application/json";
-  static constexpr auto token = "nope :)";
 
   std::string getSecurityHeader();
-  std::string getURL();
+  /**
+   * @brief Take base URL http://IP:PORT/api/ and add some
+   *
+   * @param aPath - Path To Append
+   * @return std::string - Full path to API endpoint
+   */
+  std::string getURL(std::string aPath = "");
 
-  // Callback function to handle data received from the server
-  static size_t write_callback(void *contents, size_t size, size_t nmemb,
-                               void *userp);
+  static size_t HandleResultCallback(void *contents, size_t size, size_t nmemb,
+                                     void *userp);
 
-  std::string mIp = "192.168.86.105";
-  std::string mPort = "8123";
+  const std::string mIp;
+  const std::string mPort;
 
   std::string mResponse = "";
-
-  CURL *mCurlHandle = nullptr;
 };
 
 } // namespace HomeAssistant
