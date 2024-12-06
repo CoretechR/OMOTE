e16007973fe59d4a3977a676b7cfcfb187bad8ae
Colin Edwards <colin@recursivepenguin.com>
2024-12-05 20:54:32 -0600

    Add macOS support to the simulator
    Users will need to use homebrew (https://brew.sh) to install SDL2
    `brew install sdl2`

--- Platformio/hardware/hardwareLayer.h
+++ Platformio/hardware/hardwareLayer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#if defined(WIN32) || defined(__linux__)
+#if defined(WIN32) || defined(__linux__) || defined(__APPLE__)
   #include "windows_linux/include_hal_windows_linux.h"
 #elif defined(ARDUINO)
   #include "ESP32/include_hal_esp32.h"
