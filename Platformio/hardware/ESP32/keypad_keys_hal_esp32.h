df22b457826cd7f0462444b303677e8e9e861809
KlausMu <kmusch@gmail.com>
2025-02-25 15:39:55 +0100

    added const to BUTTON_PIN_BITMASK and SW_1_GPIO to SW_5_GPIO

--- Platformio/hardware/ESP32/keypad_keys_hal_esp32.h
+++ Platformio/hardware/ESP32/keypad_keys_hal_esp32.h
@@ -3,18 +3,18 @@
 #if(OMOTE_HARDWARE_REV >= 5)
 
 #else
-    extern uint8_t SW_1_GPIO; // 1...5: Output
-    extern uint8_t SW_2_GPIO;
-    extern uint8_t SW_3_GPIO;
-    extern uint8_t SW_4_GPIO;
-    extern uint8_t SW_5_GPIO;
+    extern const uint8_t SW_1_GPIO; // 1...5: Output
+    extern const uint8_t SW_2_GPIO;
+    extern const uint8_t SW_3_GPIO;
+    extern const uint8_t SW_4_GPIO;
+    extern const uint8_t SW_5_GPIO;
     extern const uint8_t SW_A_GPIO; // A...E: Input
     extern const uint8_t SW_B_GPIO;
     extern const uint8_t SW_C_GPIO;
     extern const uint8_t SW_D_GPIO;
     extern const uint8_t SW_E_GPIO;
 #endif
-extern uint64_t BUTTON_PIN_BITMASK;
+extern const uint64_t BUTTON_PIN_BITMASK;
 
 void init_keys_HAL(void);
 void keys_getKeys_HAL(void* ptr, unsigned long currentMillis);
