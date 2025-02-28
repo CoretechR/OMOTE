25ed93ed161226b040aaca09692212aa438d4097
MatthewColvin <mcolvin32.7@gmail.com>
2025-02-27 23:16:44 -0600

    clean up stats simulator by inlining defintions

--- Platformio/HAL/Targets/Simulator/StatsSimulator.hpp
+++ Platformio/HAL/Targets/Simulator/StatsSimulator.hpp
@@ -1,21 +1,28 @@
 #pragma once
-#include "SystemStatsInterface.h"
 #include <random>
 #include <thread>
 
+#include "SystemStatsInterface.h"
+
 class StatsSimulator : public SystemStatsInterface {
-public:
-  StatsSimulator()
-      : mHeapNotifyThread([this] {
-          while (true) {
-            SendFreeHeapNotification();
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-          }
-        }){};
+ public:
+  StatsSimulator();
   ~StatsSimulator() override = default;
 
-  uint32_t GetFreeHeap() override { return std::rand() % 90000; }
+  uint32_t GetFreeHeap() override;
 
-private:
+ private:
   std::thread mHeapNotifyThread;
 };
+
+inline StatsSimulator::StatsSimulator()
+    : mHeapNotifyThread([this] {
+        while (true) {
+          SendFreeHeapNotification();
+          std::this_thread::sleep_for(std::chrono::seconds(2));
+        }
+      })
+
+{}
+
+inline uint32_t StatsSimulator::GetFreeHeap() { return std::rand() % 90000; }
