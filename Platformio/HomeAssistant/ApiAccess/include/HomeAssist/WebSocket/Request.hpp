f7d484cff1bb1b32ad45010e948ae85d1558ab74
MatthewColvin <mcolvin32.7@gmail.com>
2025-03-01 19:31:49 -0600

    add ability to set id in request

--- Platformio/HomeAssistant/ApiAccess/include/HomeAssist/WebSocket/Request.hpp
+++ Platformio/HomeAssistant/ApiAccess/include/HomeAssist/WebSocket/Request.hpp
@@ -2,22 +2,29 @@
 
 #include <string>
 
+#include "RapidJsonUtilty.hpp"
+#include "rapidjson/document.h"
+
 namespace HomeAssist::WebSocket {
 
 class Request {
  public:
+  Request(rapidjson::Document aRequestMessage);
   Request(std::string aRequestMessage);
   ~Request() = default;
 
   std::string GetRequestMessage();
 
  private:
-  std::string mRequestMessage;
+  rapidjson::Document mRequestMessage;
 };
 
-inline Request::Request(std::string aRequestMessage)
-    : mRequestMessage(aRequestMessage) {}
+inline Request::Request(std::string aRequestMessage) {
+  mRequestMessage.Parse(aRequestMessage.c_str());
+}
 
-inline std::string Request::GetRequestMessage() { return mRequestMessage; }
+inline std::string Request::GetRequestMessage() {
+  return ToString(mRequestMessage);
+}
 
 }  // namespace HomeAssist::WebSocket
