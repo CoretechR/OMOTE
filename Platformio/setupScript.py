0e2240ca93c064a8dc006ec1612c4b9ea1a8661b
MatthewColvin <mcolvin32.7@gmail.com>
2025-03-08 10:55:37 -0600

    clean up new setupscript and add dep to build-simulator

--- Platformio/setupScript.py
+++ Platformio/setupScript.py
@@ -2,17 +2,56 @@
 Import("env")
 import SCons
 import SCons.Environment
+import subprocess
 
 buildEnv : SCons.Environment.Base = env
+
+LINUX_APT_DEPENDENCES = {"libsdl2-dev","libcurl4-openssl-dev", "libboost-all-dev"}
+
+def runGitCommand(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        print(f"Error executing command: {command}")
+        print(stderr)
+        return None
+    return stdout
+
+def EnsureSubmoduleCheckout():
+  runGitCommand("git submodule update --init")
+
+def isAptInstalled(apt):
+    try:
+        result = subprocess.run(['dpkg', '-s', apt], capture_output=True, text=True)
+        return result.returncode == 0
+    except:
+        return False
+
+def installApt(apt):
+    try:
+        subprocess.run(['sudo', 'apt-get', 'install', '-y', apt], check=True)
+        print(f"Successfully installed {apt}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to install {apt}: {e}")
+
+def verifySimDependencies():   
+    try:
+        for dep in LINUX_APT_DEPENDENCES:
+          print(f"Detected Missing Dependency {dep} Installing...")
+          if(not isAptInstalled(dep)):
+            installApt(dep)
+    except: 
+       print("Failed To Verify Dependencies are Installed!")
+
+def verifyDependencies():   
+  verifySimDependencies()
 
 def PrintInfo():
     print('')
     print("PIO Build env:", buildEnv["PIOENV"])
     print("Detected Platform:", buildEnv["PLATFORM"])
     print('')
-    # print('DUMPING:')
-    # print(buildEnv.Dump())
-    # print('')
 
 def before_upload(source, target, env):
     print("before_upload")
@@ -20,21 +59,11 @@ def before_upload(source, target, env):
 def after_upload(source, target, env):
     print("after_upload")
 
-def pre_build(source,target,env):
-    print("pre MAIN build")
-
-def post_build(source, targe, env):
-    print("post MAIN BUILD")
-
-relativeBuildDir = ".pio/build/" + buildEnv["PIOENV"]
-simMain = f"{relativeBuildDir}/src/simMain.o"
-print (simMain)
-# Custom actions when building simulator
-buildEnv.AddPreAction("program", pre_build )
-buildEnv.AddPostAction("program", post_build)
 # Custom actions when launching simulator
 buildEnv.AddPreAction("upload", before_upload)
 buildEnv.AddPostAction("upload", after_upload)
 
 
 PrintInfo()
+EnsureSubmoduleCheckout()
+verifyDependencies()
