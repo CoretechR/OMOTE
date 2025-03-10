162ddc1dc6a90b4e1195dba22a870e34faba1274
Andrew Stucky <andrew.david.stucky@gmail.com>
2025-03-10 09:24:03 -0400

    chore: reverts old mqtt restore.

--- Platformio/hardware/ESP32/mqtt_hal_esp32.h
+++ Platformio/hardware/ESP32/mqtt_hal_esp32.h
@@ -1,24 +1,16 @@
 #pragma once
 
-#include <nlohmann/json.hpp>
-#include <string>
-
-using json = nlohmann::json;
-
 #if (ENABLE_WIFI_AND_MQTT == 1)
 
 void init_mqtt_HAL(void);
 bool getIsWifiConnected_HAL();
 void mqtt_loop_HAL();
-bool publishMQTTMessage_HAL(const char *topic, json payload);
+bool publishMQTTMessage_HAL(const char *topic, const char *payload);
 void wifiStop_HAL();
 
 typedef void (*tAnnounceWiFiconnected_cb)(bool connected);
-void set_announceWiFiconnected_cb_HAL(
-    tAnnounceWiFiconnected_cb pAnnounceWiFiconnected_cb);
-typedef void (*tAnnounceSubscribedTopics_cb)(std::string topic,
-                                             std::string payload);
-void set_announceSubscribedTopics_cb_HAL(
-    tAnnounceSubscribedTopics_cb pAnnounceSubscribedTopics_cb);
+void set_announceWiFiconnected_cb_HAL(tAnnounceWiFiconnected_cb pAnnounceWiFiconnected_cb);
+typedef void (*tAnnounceSubscribedTopics_cb)(std::string topic, std::string payload);
+void set_announceSubscribedTopics_cb_HAL(tAnnounceSubscribedTopics_cb pAnnounceSubscribedTopics_cb);
 
 #endif
