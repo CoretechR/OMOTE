22424f0b0584078d2f543632a11cd62551f070c1
NumericalPseudonym <84367280+NumericalPseudonym@users.noreply.github.com>
2025-04-02 02:01:11 -0400

    Adding IR Codes for Apple TV (#109)
    * Adding IR Codes for Apple TV
    
    * Fixing one code.

--- Platformio/src/devices/mediaPlayer/device_appleTV/device_appleTV.cpp
+++ Platformio/src/devices/mediaPlayer/device_appleTV/device_appleTV.cpp
@@ -3,8 +3,40 @@
 #include "applicationInternal/hardware/hardwarePresenter.h"
 #include "device_appleTV.h"
 
-uint16_t APPLETV_GUI_EVENT_USER_DATA; //"AppleTV_gui_event_user_data";
+uint16_t APPLETV_UP;
+uint16_t APPLETV_DOWN;
+uint16_t APPLETV_LEFT;
+uint16_t APPLETV_RIGHT;
+uint16_t APPLETV_OK;
+
+uint16_t APPLETV_PLAY;
+uint16_t APPLETV_PAUSE;
+
+uint16_t APPLETV_10_SECOND_BACK;
+uint16_t APPLETV_10_SECOND_FOREWARD;
+
+uint16_t APPLETV_NEXT;
+uint16_t APPLETV_PREVIOUS;
+
+uint16_t APPLETV_MENU;
+uint16_t APPLETV_HOME;
 
 void register_device_appleTV() {
-  register_command(&APPLETV_GUI_EVENT_USER_DATA           , makeCommandData(IR, {std::to_string(IR_PROTOCOL_SONY)}));  // payload must be set when calling commandHandler
+  register_command(&APPLETV_UP                   , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E15080"}));
+  register_command(&APPLETV_DOWN                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E13080"}));
+  register_command(&APPLETV_LEFT                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E19080"}));
+  register_command(&APPLETV_RIGHT                , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E16080"}));
+  register_command(&APPLETV_OK                   , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E13A80"}));
+
+  register_command(&APPLETV_PLAY                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E1FA80"})); 
+  register_command(&APPLETV_PAUSE                , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0xA7E14C80:32:1"})); // Code + kNECBits + 1 repeat
+
+  register_command(&APPLETV_10_SECOND_BACK       , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0xA7E16480:32:1"})); // Code + kNECBits + 1 repeat
+  register_command(&APPLETV_10_SECOND_FOREWARD   , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0xA7E11080:32:1"})); // Code + kNECBits + 1 repeat
+
+  register_command(&APPLETV_NEXT                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0xA7E1C480:32:1"})); // Code + kNECBits + 1 repeat
+  register_command(&APPLETV_PREVIOUS             , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0xA7E1A480:32:1"})); // Code + kNECBits + 1 repeat
+  
+  register_command(&APPLETV_MENU                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E1C080"}));
+  register_command(&APPLETV_HOME                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0xA7E10280:32:1"})); // Code + kNECBits + 1 repeat
 }
--- Platformio/src/devices/mediaPlayer/device_appleTV/device_appleTV.h
+++ Platformio/src/devices/mediaPlayer/device_appleTV/device_appleTV.h
@@ -1,5 +1,21 @@
 #pragma once
 
-extern uint16_t APPLETV_GUI_EVENT_USER_DATA;
+extern uint16_t APPLETV_UP;
+extern uint16_t APPLETV_DOWN;
+extern uint16_t APPLETV_LEFT;
+extern uint16_t APPLETV_RIGHT;
+extern uint16_t APPLETV_OK;
+
+extern uint16_t APPLETV_PLAY;
+extern uint16_t APPLETV_PAUSE;
+
+extern uint16_t APPLETV_10_SECOND_BACK;
+extern uint16_t APPLETV_10_SECOND_FOREWARD;
+
+extern uint16_t APPLETV_NEXT;
+extern uint16_t APPLETV_PREVIOUS;
+
+extern uint16_t APPLETV_MENU;
+extern uint16_t APPLETV_HOME;
 
 void register_device_appleTV();
